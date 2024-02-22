2e12ed0d39eb67cf99b97ff7ea180c212ca4d797
PizzaDumpster <djd187@hotmail.com>
2024-02-22 09:04:23 -0500

    changes

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,8 +8,8 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     [SerializeField] float speed;
     [SerializeField] Vector2 jumpForce;
-    bool movingLeft;
-    bool isGrounded; 
+    [SerializeField] bool movingRight;
+    [SerializeField] bool isGrounded; 
     [SerializeField] Transform groundCheck; 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +25,14 @@ public class PlayerController : MonoBehaviour
         isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down,0.05f); 
         if (Input.GetKey(KeyCode.D))
         {
-            movingLeft = true;
+            movingRight = true;
             transform.localScale = new Vector3(1, 1, 1);
             rb.velocity = rb.velocity + new Vector2(speed, 0);
             anim.SetTrigger("walk");
         }
         else
         {
-            if (movingLeft)
+            if (movingRight)
             {
                 rb.velocity += new Vector2(0f, 0f);
                 anim.SetTrigger("idle");
@@ -40,14 +40,14 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.A))
         {
-            movingLeft = false;
+            movingRight = false;
             transform.localScale = new Vector3(-1, 1, 1);
             rb.velocity = rb.velocity + new Vector2(-speed, 0);
             anim.SetTrigger("walk");
         }
         else
         {
-            if (!movingLeft)
+            if (!movingRight)
             {
                 rb.velocity += new Vector2(0f, 0f);
                 anim.SetTrigger("idle");
